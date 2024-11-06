80f9f35a2d74cc8099d2d41b6c54a3d225aef1c5
dgonza11 <dgonza11@estudiantes.unileon.es>
2024-11-06 13:16:32 +0100

    Finished version 1.0.0 of the frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/App.css';
+import spotify from './assets/spotify.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <nav className="navbar navbar-dark justify-content-between p-3">
+        <div className="d-flex align-items-center">
+          <img src={spotify} alt="Logo" className="logo" />
+          <span className="navbar-brand h1">SpotifyRecomender</span>
+        </div>
+        <div>
+          <button className="btn btn-outline-light me-2 nav-btn">Log In</button>
+          <button className="btn btn-outline-light nav-btn">Sign Up</button>
+        </div>
+      </nav>
+
+      <div className="main-content d-flex flex-column justify-content-center align-items-center text-center">
+        <h1 className="text-light title">¿Estás buscando algo nuevo para escuchar?</h1>
+        <button className="btn btn-success mt-4">Empezar a escuchar</button>
+      </div>
     </div>
   );
 }
